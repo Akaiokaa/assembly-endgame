f21899248b5a98b5ae510fc14556f4235d8160a6
Akaioka <brendanvillaraza@yahoo.com>
2026-05-22 13:15:55 -0700

    styling for coding languages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import { useState } from 'react'
 import './App.css'
-
+import languages from './languages.js'
 function App() {
+  const CODING_LANGUAGES = languages.map((language) => (<div
+    key={language.name}
+    className='language_name' 
+    style={{
+    "background-color": language.backgroundColor,
+    color: language.color
+  }}>{language.name}</div>))
 
+  console.log(languages)
   return (
     <>
       <main>
@@ -14,6 +22,9 @@ function App() {
       <div className='game-status'>
         <h2>You win!</h2>
         <p>Well done!</p>
+      </div>
+      <div className='coding-languages'> 
+        {CODING_LANGUAGES}
       </div>
       
     </>
