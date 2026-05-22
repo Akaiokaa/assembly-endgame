c14a2997f93d2fc844dbfe1e52e3f95f4463e30e
Akaioka <brendanvillaraza@yahoo.com>
2026-05-22 12:09:33 -0700

    first section of assembly endgame

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,17 @@ function App() {
 
   return (
     <>
+      <main>
+        <header>
+          <h2>Assembly: Endgame</h2>
+          <p>Guess the word under 8 attempts to keep the programming world safe from assembly</p>
+        </header>
+      </main>
+      <div className='game-status'>
+        <h2>You win!</h2>
+        <p>Well done!</p>
+      </div>
+      
     </>
   )
 }
